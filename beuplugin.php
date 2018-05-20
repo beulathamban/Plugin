1d35b44c823d8cda791cd2c9875189170793aa2e
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-05-20 21:28:19 +1200

    plugin.php

--- beuplugin.php
+++ beuplugin.php
@@ -1,3 +1,4 @@
+
 <?php 
 /**
 * @package BeuPlugin
@@ -27,72 +28,69 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
-
 //security
-
 defined ('ABSPATH') or die('You dont have permission to access this file');
+if ( !class_exists( 'BeuPlugin' ) ) {
 
+	class BeuPlugin
+	{
 
-class BeuPlugin
-{
-	function __construct() {
-		add_action( 'init', array( $this, 'custom_post_type' ) );
+		public $plugin;
+
+		function __construct() {
+			$this->plugin = plugin_basename( __FILE__ );
+		}
+
+		function register() {
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+
+			add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+			add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+		}
+
+		public function settings_link( $links ) {
+			$settings_link = '<a href="admin.php?page=beuplugin">Settings</a>';
+			array_push( $links, $settings_link );
+			return $links;
+		}
+
+		public function add_admin_pages() {
+			add_menu_page( 'Beula Plugin', 'Beula', 'manage_options', 'beuplugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+		}
+
+		public function admin_index() {
+			require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+		}
+
+		protected function create_post_type() {
+			add_action( 'init', array( $this, 'custom_post_type' ) );
+		}
+
+		function custom_post_type() {
+			register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
+		}
+
+		function enqueue() {
+			// enqueue all our scripts
+			wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__ ) );
+			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
+		}
+
+		function activate() {
+			require_once plugin_dir_path( __FILE__ ) . 'inc/beuplugin-activate.php';
+			BeuPluginActivate::activate();
+		}
 	}
 
-	function activate() {
-		// generated a CPT
-		$this->custom_post_type();
-		// flush rewrite rules
-		flush_rewrite_rules();
-	}
-
-	function deactivate() {
-		// flush rewrite rules
-		flush_rewrite_rules();
-	}
-
-	
-
-	function custom_post_type() {
-		register_post_type( 'book', array( 'public' => true, 'label' => 'Books' ) );
-	}
-}
-
-if ( class_exists( 'BeuPlugin' ) ) {
 	$beuPlugin = new BeuPlugin();
+	$beuPlugin->register();
+
+	// activation
+	register_activation_hook( __FILE__, array( $beuPlugin, 'activate' ) );
+
+	// deactivation
+	require_once plugin_dir_path( __FILE__ ) . 'inc/beuplugin-deactivate.php';
+	register_deactivation_hook( __FILE__, array( 'BeuPluginDeactivate', 'deactivate' ) );
+
 }
-
-// activation
-register_activation_hook( __FILE__, array( $beuPlugin, 'activate' ) );
-
-// deactivation
-register_deactivation_hook( __FILE__, array( $beuPlugin, 'deactivate' ) );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
